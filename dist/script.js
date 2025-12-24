c942ee5d55223ef23204384d5d162a8715160714
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-12-24 14:47:59 -0300

    RotaProtegida concluida

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,8 @@
 import { mostarDados } from "./fetch.js";
 import { postLogin } from "./fetchLogin.js";
+import { protectRoute } from "./protectRoute.js";
 import { AbrirFormularioCadastro } from "./showFormCreate.js";
+protectRoute();
 async function dados() {
     mostarDados();
 }
